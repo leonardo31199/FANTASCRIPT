4a59f7a3ece7cdea63776617a0d7cfbc007208ee
vostroUsername <leonardoflores.comunicazioni@gmail.com>
2025-05-05 13:01:58 +0200

    v1

---
+++ src/main/java/com/example/fantascript/FantascriptApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantascriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantascriptApplication.class, args);
+    }
+
+}
