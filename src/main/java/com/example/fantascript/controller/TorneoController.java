f7c7cf4a13d6a1b509f9b9509a9316261a690826
michele.this <michelebrugognone13@gmail.com>
2025-05-06 16:27:55 +0200

    Creazione squadre V5

---
+++ src/main/java/com/example/fantascript/controller/TorneoController.java
@@ -0,0 +1,37 @@
+package com.example.fantascript.controller;
+
+
+import com.example.fantascript.model.dto.PartitaDTO;
+import com.example.fantascript.model.entities.Partita;
+import com.example.fantascript.service.TorneoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/torneo")
+@RequiredArgsConstructor
+public class TorneoController {
+
+    private final TorneoService torneoService;
+
+    @PostMapping("/start")
+    public List<PartitaDTO>avviaTorneo()
+    {
+        List<Partita> partite = torneoService.avviaTorneo();
+
+        return partite.stream()
+                .map(p->new PartitaDTO(
+                        p.getSquadraCasa().getNome(),
+                        p.getGolCasa(),
+                        p.getSquadraTrasferta().getNome(),
+                        p.getGolTrasferta(),
+                        p.getVincitore().getNome()
+                ))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/example/fantascript/model/dto/PartitaDTO.java
@@ -0,0 +1,23 @@
+package com.example.fantascript.model.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+// Questo DTO serve per restituire al frontend
+// i risultati di una partita simulata
+// (nomi delle squadre, gol e vincitore)
+public class PartitaDTO {
+
+    private String squadraCasa;
+    private int golCasa;
+
+    private String squadraTrasferta;
+    private int goltrasferta;
+
+    private String vincitore;
+}
