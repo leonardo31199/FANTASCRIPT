637f4048f25ad039dcd9da829956e595a7cdcfde
Donato Morra <donatomorra90@gmail.com>
2025-05-06 17:17:06 +0200

    v5

--- src/main/java/com/example/fantascript/controller/GiocatoreController.java
+++ src/main/java/com/example/fantascript/controller/GiocatoreController.java
@@ -2,9 +2,13 @@ package com.example.fantascript.controller;
 
 import com.example.fantascript.model.dao.GiocatoreDAO;
 import com.example.fantascript.model.dao.SquadraDAO;
+import com.example.fantascript.model.entities.Giocatore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/fanta")
@@ -12,5 +16,12 @@ public class GiocatoreController {
 
     @Autowired
     private GiocatoreDAO gdao;
+
+    @GetMapping("/lista")
+    public List<Giocatore> lista()
+    {
+        return gdao.findAll();
+    }
+
 
 }
